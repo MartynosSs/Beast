f56b6e834bd762e90004f18756cfc73883dbc167
Микаилов Эльмир Адалятович <100854727+MartynosSs@users.noreply.github.com>
2022-05-10 17:51:17 +0300

    Add files via upload

---
+++ Beat_solo/java.js
@@ -0,0 +1,12 @@
+const openBtn = document.querySelector(".burger");
+const closeBtn = document.querySelector(".fullscreen-menu__close");
+const overLay = document.querySelector(".fullscreen-menu");
+
+
+openBtn.addEventListener("click", e => {
+    overLay.style.display = "block";
+});
+
+
+
+
